ca7adaa812b669bc001e4f001c58d72ce6b77ab3
adityamathur11 <adityamathur11@users.noreply.github.com>
2017-05-26 21:13:43 +0530

    base structure added

---
+++ controllers/Customer.js
@@ -0,0 +1,12 @@
+/**
+ * Created by Aditya on 26-May-17.
+ */
+
+var express = require('express')
+    ,fs = require('fs'),
+    JSONStream = require('JSONStream')
+    ,router = express();
+var config = require('config');
+
+
+module.exports = router;
---
+++ test/controllers/Customer.test.js
@@ -0,0 +1,8 @@
+process.env.NODE_ENV = 'test';
+
+var chai = require('chai');
+var should = chai.should();
+describe('awesome' , function () {
+    it('should do some thing' , function () {
+    });
+});
