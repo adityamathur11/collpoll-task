92713f15d2c6e7395ace8f38d5484b3cdfa698c2
adityamathur11 <adityamathur11@users.noreply.github.com>
2017-05-29 22:03:14 +0530

    controller edited

--- controllers/website.js
+++ controllers/website.js
@@ -2,8 +2,6 @@ var express = require('express')
     ,router = express();
 var path = require('path');
 
-router.use('/public',express.static(__dirname + '/public'));
-router.use('/sample' , express.static(__dirname+'/website'));
 router.get('*',function (req , res) {
     res.sendFile('index.html', { root: path.join(__dirname,'../website')});
 });
